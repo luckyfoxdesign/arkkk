783cb32a3547b0ed7fdedbec6417e48fedfbc555
ark <ark@unitkon.tld>
2023-06-30 00:43:56 +0300

    certbot done

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-domains=("ns1.firstvds.ru" "ns2.firstvds.ru")
+domains=("unitkon.app")
 email="nik.ivanov1911@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 data_path="./CERTBOT/certbot"
 rsa_key_size=4096
@@ -39,8 +39,8 @@ mkdir -p "$data_path"
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] && [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
 fi
 
 # Dummy certificate
