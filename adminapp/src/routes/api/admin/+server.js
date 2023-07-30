bd26d89615618289511954df9589a0f6ea1062d1
luckyfoxdesign <luckyfoxdesign@yandex.ru>
2023-07-30 23:35:10 +0200

    admin app removed from gitingore

---
+++ adminapp/src/routes/api/admin/+server.js
@@ -0,0 +1,23 @@
+import { json } from "@sveltejs/kit";
+
+export async function PUT({ event, url, params, request }) {
+  const reqType = url.searchParams.get("t");
+  const reqBodyData = await request.text()
+  const textWithSpaces = reqBodyData.replaceAll(/\n/g, " ")
+  let insertStatus = 0;
+
+  if (reqType === "w") {
+    insertStatus = await getResult(textWithSpaces);
+  }
+
+  return json({ status: insertStatus });
+}
+
+async function getResult(text) {
+  const resp = await fetch(`http://sc_goapp:8080/insertkv`, {
+    method: "PUT",
+    headers: { "Content-Type": "text/plain" },
+    body: text,
+  })
+  return resp.status;
+}
